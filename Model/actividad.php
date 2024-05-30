e7016e9c534cadfaffc30a9503b6e5aeccf66f37
Bryan-JR <brayanjiru14@gmail.com>
2024-05-30 18:00:41 -0500

    Gestion de proyectos, primer commit

---
+++ Model/actividad.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once(__DIR__ . '/../Model/DAO.php');
+
+class Actividad extends DAO
+{
+
+
+    protected $table = "actividades";
+    protected $ident = "idActividad";
+
+
+    public function getAll()
+    {
+        $result = $this->selectAll();
+        return $result;
+    }
+
+    public function consult($sql)
+    {
+        $result = $this->query($sql);
+        return $result;
+    }
+
+    
+    public function getById($id) 
+    {
+        $result = $this->selectId($id);
+        return $result;
+    }
+
+
+    public function insert($data) {
+        $this->store($data);
+    }
+
+
+    public function setUpdate($id, $data) {
+        $this->update($id, $data);
+    }
+
+
+    public function remove($id) {
+        $this->delete($id);
+    }
+
+    public function getEstado($param)
+    {
+        $result = $this->query("SELECT * FROM $this->table WHERE estado = '". $param."'");
+        return $result;
+    }
+    
+}
---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB", "universidad");
+define("DB_USER", "root");
+define("DB_PASS", "Qwe.123");
+
+
+set_include_path('/gestionProyectos/');
+
+?>
